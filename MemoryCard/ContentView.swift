22fcd2df04ffa08b6409cc898090b63f67dfe7d8
Truc Phan <trucphan@my.unt.edu>
2023-12-23 16:56:30 -0600

    created 4 cards, learned about struct, View, and different Stack.

--- MemoryCard/ContentView.swift
+++ MemoryCard/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
         }
         .padding()
+        .foregroundColor(.pink)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(lineWidth: 5)
+                Text("🥹").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.pink)
+            }
+            
+        }
     }
 }
 
