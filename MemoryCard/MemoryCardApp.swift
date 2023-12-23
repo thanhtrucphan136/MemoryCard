bad1e679094e554400e4e4b5b7c327a93dcc8e1c
Truc Phan <trucphan@my.unt.edu>
2023-12-23 15:45:27 -0600

    Initial Commit

---
+++ MemoryCard/MemoryCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardApp.swift
+//  MemoryCard
+//
+//  Created by Truc Phan on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
